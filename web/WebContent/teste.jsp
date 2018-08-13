700ce49b2c96337aa10b068113637259065e92ea
Quantum <Quantum@192.168.54.119>
2018-08-13 13:39:54 -0300

    teste

---
+++ web/WebContent/teste.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<form name="frmTeste" id="frmTeste" action="SendJsp" method="post"> 
+		<input type="text" name="txtTeste" id="txtTeste" />
+		<input type="submit" name="btnTeste" id="btnTeste" value="click"/>
+	</form>
+	<form name="frmTesteI" id="frmTesteI" action="AnotherTeste.jsp" method="post"> 
+		<input type="text" name="txtTeste" id="txtTeste" />
+		<input type="submit" name="btnTeste" id="btnTeste" value="click"/>
+	</form>
+</body>
+</html>
---
+++ web/src/com/dynamic/SendJsp.java
@@ -0,0 +1,42 @@
+package com.dynamic;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SendJsp
+ */
+@WebServlet("/SendJsp")
+public class SendJsp extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SendJsp() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String text = request.getParameter("txtTeste");
+		PrintWriter writer = response.getWriter();
+		writer.println(text);
+//		<input type="text" name="txtTeste" id="txtTeste" />
+//		<input type="submit" name="btnTeste" id="btnTeste" value="click"/>
+		writer.close();
+	}
+
+}
